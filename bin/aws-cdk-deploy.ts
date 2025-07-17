8c3ef3759e1b31247f829aaa0b031585563e5dc9
Fiebig <mfiebig@highline.edu>
2025-07-17 00:05:12 -0700

    Initial commit

--- bin/aws-cdk-deploy.ts
+++ bin/aws-cdk-deploy.ts
@@ -2,19 +2,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { AwsCdkDeployStack } from '../lib/aws-cdk-deploy-stack';
 
+const region = "us-west-2"
+const orgId = "o-ljx5a2fodi"
+const managementAccount = "985539764435"
+
 const app = new cdk.App();
 new AwsCdkDeployStack(app, 'AwsCdkDeployStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+    stackName: 'CdkDeployStack',
+    orgId: orgId,
+    env: {
+        account: managementAccount,
+        region: region
+    }
 });
--- lib/aws-cdk-deploy-stack.ts
+++ lib/aws-cdk-deploy-stack.ts
@@ -1,16 +1,69 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as stackset from 'cdk-stacksets'
+import {RegionConcurrencyType} from 'cdk-stacksets'
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import {Construct} from 'constructs';
+
+interface AwsCdkDeployStackProps extends cdk.StackProps {
+    orgId: string;
+}
 
 export class AwsCdkDeployStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props: AwsCdkDeployStackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const managementAccount = this.account
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkDeployQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const bucket = new s3.Bucket(this, "Assets", {});
+
+        bucket.addToResourcePolicy(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: ["s3:Get*", "s3:List*"],
+                resources: [bucket.arnForObjects("*"), bucket.bucketArn],
+                principals: [new iam.OrganizationPrincipal(props.orgId)],
+            })
+        );
+
+        new s3deploy.BucketDeployment(this, 'DeployAssets', {
+            sources: [s3deploy.Source.asset('./assets')],
+            destinationBucket: bucket,
+            extract: true
+        })
+
+        new cdk.CfnOutput(this, 'BucketName', {
+            value: bucket.bucketName,
+        })
+
+        new stackset.StackSet(this, 'StackSet', {
+            stackSetName: 'CdkDeployStackSet',
+            template: {
+                templateUrl: bucket.urlForObject('cdk-bootstrap.template.yml'),
+            },
+            capabilities: [stackset.Capability.NAMED_IAM],
+            deploymentType: stackset.DeploymentType.serviceManaged({
+                autoDeployEnabled: true,
+                delegatedAdmin: true,
+                autoDeployRetainStacks: true
+            }),
+            managedExecution: true,
+            target: stackset.StackSetTarget.fromOrganizationalUnits({
+                organizationalUnits: ['r-go9v'],
+                regions: ['us-west-2'],
+                parameterOverrides: {
+                    'TrustedAccounts': managementAccount,
+                    'TrustedAccountsForLookup': managementAccount,
+                    'CloudFormationExecutionPolicies': 'arn:aws:iam::aws:policy/AdministratorAccess'
+                },
+            }),
+            operationPreferences: {
+                maxConcurrentCount: 5,
+                regionConcurrencyType: RegionConcurrencyType.PARALLEL,
+                failureToleranceCount: 5,
+                failureTolerancePercentage: 100
+            }
+        });
+    }
 }
